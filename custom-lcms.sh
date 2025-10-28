7f8bf49ddae5577d139348d7e60418229a05e816
Kovid Goyal <kovid@kovidgoyal.net>
2025-10-28 12:55:43 +0530

    Nicer custom lcms script

--- custom-lcms.sh
+++ custom-lcms.sh
@@ -5,5 +5,10 @@
 #
 # Distributed under terms of the MIT license.
 #
-cd /t/lcms2-* && make -j8 && make install && cd - && \
-    LD_LIBRARY_PATH=/t/lc/lib go test -tags lcms2cgo -run Develop -v ./prism
+cd lcms
+dist=`pwd`/dist
+if [[ ! -d "$dist" ]]; then
+    ./configure --prefix="$dist" || exit 1
+fi
+make -j8 && make install && cd - && \
+    LD_LIBRARY_PATH=`pwd`/lcms/dist go test -tags lcms2cgo -run Develop -v ./prism
